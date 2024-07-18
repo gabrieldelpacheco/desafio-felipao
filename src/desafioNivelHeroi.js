09f4375729d2b8c68946d3a647a0bbac257efdf1
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2024-07-18 20:08:05 -0300

    Adicionado estutura de repetição do while

--- src/desafioNivelHeroi.js
+++ src/desafioNivelHeroi.js
@@ -1,25 +1,28 @@
 let nomeHeroi = "Gabriel";
-let xpDoHeroi = 10001;
+let xpDoHeroi = 5000;
 let nivel ="está no nivel de ";
 let mensagemHeroi = "O Herói de nome ";
-
-if (xpDoHeroi < 1000) {
-    console.log(mensagemHeroi +" "+ nomeHeroi+" "+nivel+" ferro.");
-} else if(xpDoHeroi >=1001 && xpDoHeroi <= 2000){
-    console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" bronze.");
-} else if(xpDoHeroi >=2001 && xpDoHeroi <=5000) {
-    console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" prata.");
-} else if(xpDoHeroi >= 6001 && xpDoHeroi <= 7000){
-    console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" ouro.");
-} else if(xpDoHeroi >= 7001 && xpDoHeroi <= 8000){
-    console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" platina.");
-} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
-    console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" ascendente.");
-} else if(xpDoHeroi >= 9001 && xpDoHeroi <= 10000){
-    console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" imortal.");
-} else {
-    console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" radiante.");
-}
+let contador = 0;
+do {
+    if (xpDoHeroi < 1000) {
+        console.log(mensagemHeroi +" "+ nomeHeroi+" "+nivel+" ferro.");
+    } else if(xpDoHeroi >=1001 && xpDoHeroi <= 2000){
+        console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" bronze.");
+    } else if(xpDoHeroi >=2001 && xpDoHeroi <=5000) {
+        console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" prata.");
+    } else if(xpDoHeroi >= 6001 && xpDoHeroi <= 7000){
+        console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" ouro.");
+    } else if(xpDoHeroi >= 7001 && xpDoHeroi <= 8000){
+        console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" platina.");
+    } else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+        console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" ascendente.");
+    } else if(xpDoHeroi >= 9001 && xpDoHeroi <= 10000){
+        console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" imortal.");
+    } else {
+        console.log(mensagemHeroi+" "+nomeHeroi+" "+nivel+" radiante.");
+    }
+    contador++;
+} while (contador == 0);
 
 /* menor que 1.000 = ferro;
 entre 1.001 e 2.000 = bronze;
